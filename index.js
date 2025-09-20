a7f55444735fac76ae31d5c277b0d54a4e3e3b0c
GitHub Action <action@github.com>
2025-09-20 09:06:07 +0000

    🗑️ Bersihkan repository - hapus semua file kecuali .github
      - Menghapus semua file dan folder
      - Mempertahankan folder .github untuk GitHub Actions
      - Dibersihkan pada: Sat Sep 20 09:06:07 UTC 2025

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  // redirect root requests to the serverless function, preserve query string and path
-  const q = req.url || '';
-  res.writeHead(307, { Location: '/api/function/proxy' + q });
-  res.end();
-};
